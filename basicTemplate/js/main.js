9f2515222767978cadf7d14ce0494cf7cda559a2
Chris Teague <cteague1222@gmail.com>
2020-09-27 13:13:53 -0500

    Set up basic game template

--- basicTemplate/js/main.js
+++ basicTemplate/js/main.js
@@ -1,9 +1,9 @@
-const game;
+let game;
 window.onload = function () {
     var config = {
-        type: Phaser.Auto, // what graphics mode to use
+        type: Phaser.Auto, // what graphics mode to use: Phaser.Canvas, Phaser.Web, or Phaser.Auto
         width: 480,
-        height: 640,
+        height: 580,
         parent: 'phaser-game', // div tag name, minus #
         scene: [SceneMain] // name of scene (game screen)
     };
