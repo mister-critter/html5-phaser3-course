c369050878b38e090814211c2b8e944ec52a789c
Chris Teague <cteague1222@gmail.com>
2020-10-04 11:07:49 -0500

    Start Road Rush project

---
+++ cars/js/sceneMain.js
@@ -0,0 +1,17 @@
+class SceneMain extends Phaser.Scene {
+    constructor() {
+        super('SceneMain'); // must match class name
+    }
+    preload() { // load resources before use
+
+    }
+    create() { // defining objects on screen
+        console.log("Ready!");
+    }
+    update() { // constant running loop that checks things like health, collision, etc
+
+    }
+    customFunctions() {
+
+    }
+}
